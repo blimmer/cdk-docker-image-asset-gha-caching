3f4f13c752acdf6c2fd59d69bb3596c1ce792160
Ben Limmer <ben@benlimmer.com>
2024-04-08 16:14:58 -0600

    feat: show docker image caching in GitHub actions

--- bin/demo.ts
+++ bin/demo.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CachedDockerImageStack } from '../lib/CachedDockerImageStack';
+import "source-map-support/register";
+import { App } from "aws-cdk-lib";
+import { CachedDockerImageStack } from "../lib/CachedDockerImageStack";
+import { AppStagingSynthesizer } from "@aws-cdk/app-staging-synthesizer-alpha";
+import { BucketEncryption } from "aws-cdk-lib/aws-s3";
 
-const app = new cdk.App();
+const app = new App({
+  defaultStackSynthesizer: AppStagingSynthesizer.defaultResources({
+    appId: "CachedDockerImageDemo",
+    stagingBucketEncryption: BucketEncryption.S3_MANAGED,
+    imageAssetVersionCount: 10, // Keep 10 latest images
+  }),
+});
+
 new CachedDockerImageStack(app);
--- lib/CachedDockerImageStack.ts
+++ lib/CachedDockerImageStack.ts
@@ -1,15 +1,33 @@
-import { Stack } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack } from "aws-cdk-lib";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
+import { Construct } from "constructs";
+import { join } from "path";
 
 export class CachedDockerImageStack extends Stack {
   constructor(scope: Construct) {
-    super(scope, 'CachedDockerImageStack');
+    super(scope, "CachedDockerImageStack");
 
-    // The code that defines your stack goes here
+    const dockerImage = new DockerImageAsset(this, "DockerImage", {
+      directory: join(__dirname, "docker"),
+      assetName: "example-docker-image",
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDockerImageAssetGhaCachingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      ...(isCi()
+        ? {
+            cacheTo: {
+              type: "gha",
+              params: { mode: "max" },
+            },
+            cacheFrom: [{ type: "gha" }],
+            outputs: ["type=docker"], // equivalent to `--load`, which exports the image to the local Docker daemon
+          }
+        : {}),
+    });
+
+    // use the docker image asset however you'd like (e.g., in ECS, Lambda, etc.)
   }
+}
+
+function isCi(): boolean {
+  // CI=true is set by GitHub Actions, CircleCI, etc.
+  return process.env.CI !== undefined;
 }
